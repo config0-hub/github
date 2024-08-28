d526234a2ac84e11800c93185113859ead969308
gary <gary>
2024-08-28 10:17:38 +0800

    rendered and updated 08-28-2024-10-17

---
+++ execgroups/_config0_configs/pull_request/_chrootfiles/var/tmp/terraform/main.tf
@@ -0,0 +1,14 @@
+provider "github" {}
+
+# if you want to get the body from a file instead
+#data "local_file" "default" {
+#  filename = var.file_path
+#}
+
+resource "github_repository_pull_request" "default" {
+    base_repository = var.repository
+    base_ref        = var.base_branch
+    head_ref        = var.feature_branch
+    title           = "PR for ${var.feature_branch} to ${var.base_branch}"
+    body            = var.body
+}
