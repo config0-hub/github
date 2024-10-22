9688080106be44dbae99eb20d54a359e36c2ebed
gary <gary>
2024-10-22 10:02:17 +0800

    rendered and updated 10-22-2024-10-02

--- execgroups/_config0_configs/add_webhook/_chrootfiles/var/tmp/terraform/variables.tf
+++ execgroups/_config0_configs/add_webhook/_chrootfiles/var/tmp/terraform/variables.tf
@@ -4,4 +4,4 @@ variable "secret" { default = "secret123" }
 variable "active" { default = true }
 variable "content_type" { default = "json" }
 variable "repository" { default = "config0-private-test" }
-variable "events" { default = "push,pull_request,issue_comment" }
+variable "events" { default = "push,pull_request" }
