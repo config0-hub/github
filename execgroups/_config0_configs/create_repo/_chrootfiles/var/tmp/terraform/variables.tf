553c4cd31376ae55a073c426207f46d6221fa476
gary <gary>
2024-12-17 11:31:07 +0800

    rendered and updated 12-17-2024-11-31

---
+++ execgroups/_config0_configs/create_repo/_chrootfiles/var/tmp/terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "repository" { default = "private-test" }
+variable "visibility" { default = "private" }
+variable "has_issues" { default = true }
+variable "has_projects" { default = false }
+variable "has_wiki" { default = false }
+variable "delete_branch_on_merge" { default = true }
+variable "default_branch" { default = "main" }
